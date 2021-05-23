080f0adf534ed044b845c298266ebe4e718a6c79
shuchan-dev <robbyhafed17@gmail.com>
2021-05-23 18:24:27 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: "localhost",
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "172.31.36.19",
     // Cross-origin resource sharing (CORS)
     // Contoh penggunaannya *(kita hanya perlu memberikan nilai header ‘Access-Control-Allow-Origin’ dengan nilai origin luar yang akan mengkonsumsi datanya (aplikasi client).
-    // response.header('Access-Control-Allow-Origin', 'http://ec2-13-212-153-62.ap-southeast-1.compute.////amazonaws.com:8000/'); )*
+    // response.header('Access-Control-Allow-Origin', '<link atau alamat>'); )*
     // Atau Anda bisa menggunakan tanda * pada nilai origin untuk memperbolehkan data dikonsumsi oleh seluruh origin
     // response.header('Access-Control-Allow-Origin', '*');
     // Penerapannya akan jauh lebih mudah bila menggunakan Hapi. Dengan Hapi, CORS dapat ditetapkan pada spesifik route dengan menambahkan properti options.cors di konfigurasi route.js nya.
